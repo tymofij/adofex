63ac921549b2cf3d91de8412a2f9fbe33a1b9110
Tim Babych <tim.babych@gmail.com>
2011-08-31 10:44:55 +0300

    auto-outsource projects to Mozilla

---
+++ adofex/addons/impala/handlers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.db.models import get_model
+from django.conf import settings
+from transifex.projects.signals import project_created, project_deleted
+from transifex.projects.models import Project
+
+
+def outsource_to_mozilla(sender, **kwargs):
+    """
+    Outsources translation of the newborn project to general Mozilla project
+    """
+    try:
+        mozilla = Project.objects.get(slug='mozilla')
+        sender.outsource = mozilla
+        sender.save()
+    except Project.DoesNotExist:
+        pass
+
+
+def connect():
+    project_created.connect(outsource_to_mozilla)
