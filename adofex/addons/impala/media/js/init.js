f1853565b59d6fa5b3723165be27135c1a3b9ff9
Tim Babych <tim.babych@gmail.com>
2011-08-31 11:07:31 +0300

    exclude mozilla from projectlist

--- adofex/addons/impala/media/js/init.js
+++ adofex/addons/impala/media/js/init.js
@@ -1,16 +1,17 @@
 // sets initial footer position, updates it on resizes
-$(window).bind("load", function() {
-  positionFooter();
 
-  function positionFooter() {
+function positionFooter() {
     // Lotte breaks it, loading content afterwards
     // and I do not want to put listeners there
     if (typeof lotteStatus == "undefined")
         $("#footer").css({
-           position:
-             $(document.body).height() < $(window).height() ? "fixed" : "relative"
-           })
-  }
+            position:
+                $(document.body).height() < $(window).height() ? "fixed" : "relative"
+        })
+}
+
+$(window).bind("load", function() {
+  positionFooter();
 
   $(window).resize(positionFooter)
 });
