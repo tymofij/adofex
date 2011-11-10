17453b2d5d09fb95084defc8d1586277ee6b67de
Tim Babych <tim.babych@gmail.com>
2011-11-11 01:57:02 +0200

    added info on watched projects

--- adofex/addons/impala/common.py
+++ adofex/addons/impala/common.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q
+from django.db.models import Q, Sum
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
@@ -26,6 +26,9 @@ from transifex.txcommon.log import logger
 from transifex.txcommon.haystack_utils import prepare_solr_query_string, \
     fulltext_fuzzy_match_filter
 
+from transifex.releases.models import Release, RELEASE_ALL_DATA
+from transifex.resources.models import Resource, RLStats, _aggregate_rlstats
+
 @csrf_protect
 def index(request):
     """ Adds Watched and Maintained projects lists for logged in user
@@ -33,9 +36,26 @@ def index(request):
     if request.user.is_authenticated():
         maintained_projects = Project.objects.maintained_by(request.user)
         watched_projects = Project.objects.watched_by(request.user)
+        user_language_ids = request.user.team_set.all().values_list('language', flat=True)
     else:
-        maintained_projects = None
-        watched_projects = None
+        maintained_projects = []
+        watched_projects = []
+
+
+    for project in watched_projects:
+        try:
+            release = Release.objects.get(slug=RELEASE_ALL_DATA['slug'],
+                    project=project)
+            total = Resource.objects.filter(releases=release).aggregate(
+                total=Sum('total_entities'))['total']
+
+            statlist = _aggregate_rlstats(
+                RLStats.objects.filter(language__in=user_language_ids
+                ).by_release(release).order_by('language__code'),
+                'language', total)
+            setattr(project, 'statlist', statlist)
+        except:
+            raise
 
     return render_to_response("index.html",
         {'form': RememberMeAuthForm(),
