dec2082ea8fa36d80f12d6faba03cdb1057f219b
Tim Babych <tim.babych@gmail.com>
2012-11-05 02:08:53 +0200

    smoother login experience, removed extra links

--- adofex/addons/impala/common.py
+++ adofex/addons/impala/common.py
@@ -1,70 +1,10 @@
-import datetime
-from django.conf import settings
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q, Sum
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
-from django.utils import simplejson
-from django.utils.translation import ugettext as _
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic import list_detail
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from notification import models as notification
-
-from actionlog.models import LogEntry, action_logging
-from transifex.languages.models import Language
-from transifex.teams.models import Team
-from transifex.projects.models import Project
-from transifex.simpleauth.forms import RememberMeAuthForm
-from transifex.txcommon.filters import LogEntryFilter
-from transifex.txcommon.log import logger
-
-from transifex.releases import RELEASE_ALL_DATA
-from transifex.releases.models import Release
-from transifex.resources.models import Resource, RLStats, _aggregate_rlstats
-
-@csrf_protect
 def index(request):
-    """ Adds Watched and Maintained projects lists for logged in user
+    """ Dashboard. Redirects user to Projects or My projects page
     """
     if request.user.is_authenticated():
-        maintained_projects = Project.objects.maintained_by(request.user)
-        watched_projects = Project.objects.watched_by(request.user)
-        user_language_ids = Team.objects.filter(
-            Q(members=request.user)|Q(coordinators=request.user)
-            ).values_list('language', flat=True)
+        return HttpResponseRedirect(reverse("myprojects"))
     else:
-        maintained_projects = []
-        watched_projects = []
-
-
-    for project in watched_projects:
-        try:
-            release = Release.objects.get(slug=RELEASE_ALL_DATA['slug'],
-                    project=project)
-            total = Resource.objects.filter(releases=release).aggregate(
-                total=Sum('total_entities'))['total']
-
-            statlist = _aggregate_rlstats(
-                RLStats.objects.filter(language__in=user_language_ids
-                ).by_release(release).order_by('language__code'),
-                'language', total)
-            setattr(project, 'statlist', statlist)
-        except:
-            raise
-
-    return render_to_response("index.html",
-        {'form': RememberMeAuthForm(),
-         'next': request.path,
-         'num_projects': Project.objects.count(),
-         'num_languages': Language.objects.count(),
-         'num_users': User.objects.count(),
-         'maintained_projects': maintained_projects,
-         'watched_projects': watched_projects,
-        },
-        context_instance = RequestContext(request))
+        return HttpResponseRedirect(reverse("project_list"))
--- adofex/addons/impala/static/impala/js/init.js
+++ adofex/addons/impala/static/impala/js/init.js
@@ -10,3 +10,11 @@ $(document).ready(positionFooter);
 $(window).resize(positionFooter)
 // dynamic content might change page height quite unexpectedly
 setInterval(positionFooter, 750)
+
+// hack to make people who log in from home page (projects) go directly to dashboard
+$(function(){
+    var a = $("#header_secnav > a").first()
+    if (a.attr('href') == "/accounts/signin/?next=/projects/") {
+        a.attr('href', "/accounts/signin/?next=/projects/myprojects/")
+    }
+})
