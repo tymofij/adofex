47b660bb7912f11df11a1a64fb54dc33528856c8
Tim Babych <tim.babych@gmail.com>
2012-11-08 19:38:08 +0200

    migrate everybody

--- adofex/addons/legacy/management/commands/migrate_users.py
+++ adofex/addons/legacy/management/commands/migrate_users.py
@@ -2,20 +2,11 @@
 
 import sys
 from datetime import datetime
-from transifex.projects.models import Project
-from transifex.languages.models import Language
 
 from django.core.management.base import BaseCommand, CommandError
-from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
-from django.utils.encoding import smart_unicode
-from legacy import models as legacy
 from django.db import transaction, IntegrityError
 
-from userena.models import UserenaSignup
-from userena import signals as userena_signals
-from userena import settings as userena_settings
-
+from legacy import models as legacy
 from legacy.actions import migrate_user
 
 class Command(BaseCommand):
@@ -24,7 +15,7 @@ class Command(BaseCommand):
     @transaction.commit_manually
     def handle(self, **options):
         i = 0
-        for u in legacy.User.objects.exclude(username__in=('june', 'seaousak')).filter(username='tymofiy'):
+        for u in legacy.User.objects.exclude(username__in=('june', 'seaousak')): # TODO: duplicate usernames
             migrate_user(u.username)
 
             i += 1
