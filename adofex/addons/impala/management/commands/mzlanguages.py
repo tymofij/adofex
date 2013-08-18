4003291f661f3a1af3c1040a278dc005772368d3
Tim Babych <tim.babych@gmail.com>
2013-08-18 17:52:01 +0300

    add several new languages, from fresh Firefox Aurora src and on Nightingdale request

--- adofex/addons/impala/management/commands/mzlanguages.py
+++ adofex/addons/impala/management/commands/mzlanguages.py
@@ -1,30 +1,34 @@
 # -*- coding: UTF-8 -*-
+from django.conf import settings
 from django.core.management.base import NoArgsCommand, CommandError
 from django.db import transaction, IntegrityError
+from django.utils import simplejson
 
 import os
 from transifex.languages.models import Language
+from transifex.languages.management.commands.txlanguages import _DEFAULT_FIXTURE, fill_language_data
+
+mz_codes = open(os.path.join(os.path.dirname(__file__), 'all-locales')).read().split()
+# languages not present even in the fixture
+missing_langs = {
+    'dv': 'Maldivian',
+    'dsb': 'Lower Sorbian',
+    'ach': 'Acholi',
+    'ff': 'Pulaar-Fulfulde',
+    'lij': 'Ligurian',
+    'sah': 'Sakha',
+}
+tx_data = simplejson.load(file(os.path.abspath(os.path.join(settings.TX_ROOT, _DEFAULT_FIXTURE))))
 
 class Command(NoArgsCommand):
     help = "Remove languages not used by Mozilla, tweak codes, add missing"
 
-    missing_langs = {
-        'lg': 'Luganda',
-        'dsb': 'Lower Sorbian',
-    }
-
+    @transaction.commit_on_success
     def handle_noargs(self, **options):
-        codes = open(os.path.join(os.path.dirname(__file__), 'all-locales')
-            ).read().split()
-        codes.extend([
-            "en-US", # US English, not included because it is default
-            "hsb", # Upper Sorbian
-            "ms", # Malay - see bug 286080
-            ])
-
+        # this is to be run after txlanguages, doing cleanup
         for l in Language.objects.all():
             # already there
-            if l.code in codes:
+            if l.code in mz_codes or l.code in missing_langs.keys():
                 print "Found %s" % l
                 continue
             # Mozilla uses dashes, not underscores
@@ -36,8 +40,36 @@ class Command(NoArgsCommand):
             # probably some unneeded double, like af_ZA - af is enough
             print "Deleting %s" % l
             l.delete()
-        # Transifex at the moment does not have Luganda language in fixture
-        for (code, name) in self.missing_langs.items():
+
+        # fill in missing languages from fixture
+        fill_the_database_verbose(tx_data)
+
+        # fill in missing languages from list
+        for (code, name) in missing_langs.items():
             if not Language.objects.filter(code=code).exists():
                 print "Adding {} ({})".format(name, code)
                 Language(name=name, code=code).save()
+
+        # check that all moz ones are present
+        for code in mz_codes:
+            try:
+                Language.objects.get(code=code)
+            except Language.DoesNotExist:
+                print "{} is still missing".format(code)
+
+
+@transaction.commit_on_success
+def fill_the_database_verbose(data):
+    """
+    Update the language object and be verbose about it.
+    """
+    for obj in data:
+        if obj['fields']['code'] not in mz_codes:
+            continue
+        fields = obj['fields']
+        lang, created = Language.objects.get_or_create(code=fields['code'])
+        if created:
+            print (u'Creating %s language (%s)' % (fields['name'], fields['code'])).encode('UTF-8')
+        else:
+            print (u'Updating %s language (%s)' % (fields['name'], fields['code'])).encode('UTF-8')
+        fill_language_data(lang, fields)
