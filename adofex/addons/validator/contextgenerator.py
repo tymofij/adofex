153c1b5b46391f21675566bad189f6a9daf48ae5
Tim Babych <tim.babych@gmail.com>
2017-07-16 03:08:51 +0200

    bundle in validator

---
+++ adofex/addons/validator/contextgenerator.py
@@ -0,0 +1,121 @@
+from StringIO import StringIO
+import unicodehelper
+
+
+INFINITY = float("inf")
+
+
+class ContextGenerator:
+    """The context generator creates a line-by-line mapping of all files that
+    are validated. It will then use that to help produce useful bits of code
+    for errors, warnings, and the like."""
+
+    def __init__(self, data=None):
+        if isinstance(data, StringIO):
+            data = data.getvalue()
+
+        self.data = data.split("\n")
+
+    def get_context(self, line=1, column=0):
+        "Returns a tuple containing the context for a line"
+
+        line -= 1  # The line is one-based
+
+        # If there is no data in the file, there can be no context.
+        datalen = len(self.data)
+        if datalen <= line:
+            return None
+
+        build = [self.data[line]]
+
+        # Add surrounding lines if they're available. There must always be
+        # three elements in the context.
+        if line > 0:
+            build.insert(0, self.data[line - 1])
+        else:
+            build.insert(0, None)
+
+        if line < datalen - 1:
+            build.append(self.data[line + 1])
+        else:
+            build.append(None)
+
+        leading_counts = []
+
+        # Count whitespace to determine how much needs to be stripped.
+        lstrip_count = INFINITY
+        for line in build:
+            # Don't count empty/whitespace-only lines.
+            if line is None or not line.strip():
+                leading_counts.append(lstrip_count)
+                continue
+
+            # Isolate the leading whitespace.
+            ws_count = len(line) - len(line.lstrip())
+            leading_counts.append(ws_count)
+            if ws_count < lstrip_count:
+                lstrip_count = ws_count
+
+        # If all of the lines were skipped over, it means everything was
+        # whitespace.
+        if lstrip_count == INFINITY:
+            return ("", "", "")
+
+        for lnum in range(3):
+            # Skip edge lines.
+            if not build[lnum]:
+                continue
+
+            line = build[lnum].strip()
+
+            # Empty lines stay empty.
+            if not line:
+                build[lnum] = ""
+                continue
+
+            line = self._format_line(line, column=column, rel_line=lnum)
+            line = "%s%s" % (" " * (leading_counts[lnum] - lstrip_count), line)
+
+            build[lnum] = line
+
+        # Return the final output as a tuple.
+        return tuple(build)
+
+    def _format_line(self, data, column=0, rel_line=1):
+        "Formats a line from the data to be the appropriate length"
+        line_length = len(data)
+
+        if line_length > 140:
+            if rel_line == 0:
+                # Trim from the beginning
+                data = "... %s" % data[-140:]
+            elif rel_line == 1:
+                # Trim surrounding the error position
+                if column < 70:
+                    data = "%s ..." % data[:140]
+                elif column > line_length - 70:
+                    data = "... %s" % data[-140:]
+                else:
+                    data = "... %s ..." % data[column - 70:column + 70]
+
+            elif rel_line == 2:
+                # Trim from the end
+                data = "%s ..." % data[:140]
+
+        data = unicodehelper.decode(data)
+        return data
+
+    def get_line(self, position):
+        "Returns the line number that the given string position is found on"
+
+        datalen = len(self.data)
+        count = len(self.data[0])
+        line = 1
+        while count < position:
+            if line >= datalen:
+                break
+            count += len(self.data[line]) + 1
+            line += 1
+
+        return line
+
---
+++ adofex/addons/validator/xpi.py
@@ -0,0 +1,83 @@
+from zipfile import ZipFile
+from StringIO import StringIO
+
+
+def to_utf8(s):
+    if isinstance(s, unicode):
+        s = s.encode('utf-8')
+    return s
+
+
+class XPIManager(object):
+    """
+    An XPI reader and management class. Allows fun things like reading,
+    listing, and extracting files from an XPI without you needing to
+    worry about things like zip files or IO.
+    """
+
+    def __init__(self, package, mode="r", name=None, subpackage=False):
+        "Create a new managed XPI package"
+
+        self.zf = ZipFile(package, mode=mode)
+
+        # Store away the filename for future use.
+        self.filename = name or package
+        self.extension = self.filename.split(".")[-1]
+        self.subpackage = subpackage
+
+        self.contents_cache = None
+
+    def __iter__(self):
+        return (name for name in self.zf.namelist())
+
+    def __contains__(self, item):
+        return item in self.zf.namelist()
+
+    def info(self, name):
+        """Get info on a single file."""
+        return self.package_contents()[name]
+
+    def package_contents(self):
+        "Returns a dictionary of file information"
+
+        if self.contents_cache:
+            return self.contents_cache
+
+        # Get a list of ZipInfo objects.
+        files = self.zf.infolist()
+        out_files = {}
+
+        # Iterate through each file in the XPI.
+        for file_ in files:
+
+            file_doc = {"name": file_.filename,
+                        "size": file_.file_size,
+                        "name_lower": file_.filename.lower()}
+
+            file_doc["extension"] = file_doc["name_lower"].split(".")[-1]
+
+            out_files[file_.filename] = file_doc
+
+        self.contents_cache = out_files
+        return out_files
+
+    def read(self, filename):
+        "Reads a file from the archive and returns a string."
+
+        data = self.zf.read(filename)
+        return data
+
+    def write(self, name, data):
+        """Write a blob of data to the XPI manager."""
+        if isinstance(data, StringIO):
+            self.zf.writestr(name, data.getvalue())
+        else:
+            self.zf.writestr(name, to_utf8(data))
+
+    def write_file(self, name, path=None):
+        """Write the contents of a file from the disk to the XPI."""
+
+        if path is None:
+            path = name
+
+        self.zf.write(path, name)
